b1c739636d03d7358ddce3c2bec1b19c171bf456
Nikola Terziev <nikolakterziev@gmail.com>
2021-06-24 10:58:36 +0300

    Fix mappings

--- src/staking_rewards.ts
+++ src/staking_rewards.ts
@@ -1,7 +1,7 @@
 import { ServiceProvider } from '../generated/templates'
 import {ServiceProviderWhitelisted} from "../generated/ServiceProvider/StakingRewards";
 
-export function handleNewExchange(event: ServiceProviderWhitelisted): void {
+export function handleNewServiceProviderWhitelisted(event: ServiceProviderWhitelisted): void {
     // Start indexing the exchange; `event.params.exchange` is the
     // address of the new exchange contract
     ServiceProvider.create(event.params.serviceProvider)
